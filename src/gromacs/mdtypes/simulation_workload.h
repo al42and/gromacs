al42and/gromacs

commit 24c8e7df4f92fcf1caad5b89dd6316d8ef1a647b

Add simulationWorkload.haveGpuPmeOnPpRank to simplify code
Refs #3936

src/gromacs/mdtypes/simulation_workload.h
@@ -207,6 +207,12 @@ public:
     bool useMts = false;
     // Whether a GPU graph should be used to execute steps in the MD loop if run conditions allow.
     bool useMdGpuGraph = false;
+
+    // Whether PME GPU is active on this PP rank (note that currently only PP ranks use SimulationWorkload)
+    __attribute__((always_inline)) bool haveGpuPmeOnPpRank() const
+    {
+        return useGpuPme && !haveSeparatePmeRank;
+    }
 };
 
 class MdrunScheduleWorkload

src/gromacs/mdlib/mdgraph_gpu_impl.cu
@@ -60,7 +60,7 @@ MdGpuGraph::Impl::Impl(const DeviceStreamManager& deviceStreamManager,
     launchStreamAlternate_(
             new DeviceStream(deviceStreamManager.context(), DeviceStreamPriority::Normal, false)),
     havePPDomainDecomposition_(simulationWork.havePpDomainDecomposition),
-    useGpuPme_(simulationWork.useGpuPme),
+    haveGpuPmeOnThisPpRank_(simulationWork.haveGpuPmeOnPpRank()),
     haveSeparatePmeRank_(simulationWork.haveSeparatePmeRank),
     mpiComm_(mpiComm),
     evenOrOddStep_(evenOrOddStep),
@@ -263,7 +263,7 @@ void MdGpuGraph::Impl::endRecord()
     GMX_ASSERT(graphState_ == GraphState::Recording,
                "Graph should be in a recording state before recording is ended");
 
-    if (useGpuPme_ && !haveSeparatePmeRank_)
+    if (haveGpuPmeOnThisPpRank_)
     {
         // Join PME stream to NB local stream on each rank
         helperEvent_->markEvent(deviceStreamManager_.stream(gmx::DeviceStreamType::Pme));
@@ -426,7 +426,7 @@ void MdGpuGraph::Impl::launchGraphMdStep(GpuEventSynchronizer* xUpdatedOnDeviceE
 
         // If PME on same rank, sync PME (to ensure no race condition with clearing)
         // Note that separate rank PME has implicit sync, including clearing.
-        if (useGpuPme_ && !haveSeparatePmeRank_)
+        if (haveGpuPmeOnThisPpRank_)
         {
             helperEvent_->markEvent(deviceStreamManager_.stream(gmx::DeviceStreamType::Pme));
             helperEvent_->enqueueWaitEvent(*thisLaunchStream);

src/gromacs/mdlib/mdgraph_gpu_impl.h
@@ -168,8 +168,8 @@ private:
     std::unique_ptr<DeviceStream> launchStreamAlternate_;
     //! Whether PP domain decomposition is in use
     bool havePPDomainDecomposition_;
-    //! Whether GPU is used for PME
-    bool useGpuPme_;
+    //! Whether there is PME GPU task on this PP rank
+    bool haveGpuPmeOnThisPpRank_;
     //! Whether PME is handled on a separate rank
     bool haveSeparatePmeRank_;
     //! MPI communicator for PP domain decomposition

src/gromacs/mdlib/sim_util.cpp
@@ -1008,13 +1008,13 @@ static StepWorkload setupStepWorkload(const int                     legacyFlags,
     }
     flags.useGpuXBufferOps = simulationWork.useGpuXBufferOps && !flags.doNeighborSearch;
     // on virial steps the CPU reduction path is taken
-    flags.useGpuFBufferOps       = simulationWork.useGpuFBufferOps && !flags.computeVirial;
-    const bool rankHasGpuPmeTask = simulationWork.useGpuPme && !simulationWork.haveSeparatePmeRank;
-    flags.useGpuPmeFReduction    = flags.computeSlowForces && flags.useGpuFBufferOps
-                                && (rankHasGpuPmeTask || simulationWork.useGpuPmePpCommunication);
+    flags.useGpuFBufferOps = simulationWork.useGpuFBufferOps && !flags.computeVirial;
+    flags.useGpuPmeFReduction =
+            flags.computeSlowForces && flags.useGpuFBufferOps
+            && (simulationWork.haveGpuPmeOnPpRank() || simulationWork.useGpuPmePpCommunication);
     flags.useGpuXHalo              = simulationWork.useGpuHaloExchange && !flags.doNeighborSearch;
     flags.useGpuFHalo              = simulationWork.useGpuHaloExchange && flags.useGpuFBufferOps;
-    flags.haveGpuPmeOnThisRank     = rankHasGpuPmeTask && flags.computeSlowForces;
+    flags.haveGpuPmeOnThisRank     = simulationWork.haveGpuPmeOnPpRank() && flags.computeSlowForces;
     flags.computePmeOnSeparateRank = simulationWork.haveSeparatePmeRank && flags.computeSlowForces;
     flags.combineMtsForcesBeforeHaloExchange =
             (flags.computeForces && simulationWork.useMts && flags.computeSlowForces
@@ -1281,7 +1281,7 @@ static void setupLocalGpuForceReduction(const gmx::MdrunScheduleWorkload* runSch
     GpuEventSynchronizer*   pmeSynchronizer     = nullptr;
     bool                    havePmeContribution = false;
 
-    if (runScheduleWork->simulationWork.useGpuPme && !runScheduleWork->simulationWork.haveSeparatePmeRank)
+    if (runScheduleWork->simulationWork.haveGpuPmeOnPpRank())
     {
         pmeForcePtr = pme_gpu_get_device_f(pmedata);
         if (pmeForcePtr)
@@ -1422,11 +1422,13 @@ void do_force(FILE*                               fplog,
         // TODO refactor this to do_md, after partitioning.
         stateGpu->reinit(mdatoms->homenr,
                          getLocalAtomCount(cr->dd, *mdatoms, simulationWork.havePpDomainDecomposition));
-        if (stepWork.haveGpuPmeOnThisRank)
-        {
-            // TODO: This should be moved into PME setup function ( pme_gpu_prepare_computation(...) )
-            pme_gpu_set_device_x(fr->pmedata, stateGpu->getCoordinates());
-        }
+    }
+
+    if (stepWork.doNeighborSearch && simulationWork.haveGpuPmeOnPpRank())
+    {
+        GMX_ASSERT(gmx::needStateGpu(simulationWork), "StatePropagatorDataGpu is needed");
+        // TODO: This should be moved into PME setup function ( pme_gpu_prepare_computation(...) )
+        pme_gpu_set_device_x(fr->pmedata, stateGpu->getCoordinates());
     }
 
     auto* localXReadyOnDevice =

src/gromacs/mdlib/tests/mdgpugraph.cpp
@@ -78,6 +78,7 @@ SimulationWorkload makeSimulationWorkForMdGpuGraph()
     simulationWork.useGpuUpdate              = true;
     simulationWork.havePpDomainDecomposition = false;
     simulationWork.useGpuPme                 = false;
+    simulationWork.haveSeparatePmeRank       = false;
     return simulationWork;
 }
 

src/gromacs/mdtypes/state_propagator_data_gpu.h
@@ -77,9 +77,9 @@ namespace gmx
  */
 inline bool needStateGpu(SimulationWorkload simulationWorkload)
 {
-    return (simulationWorkload.useGpuPme && !simulationWorkload.haveSeparatePmeRank)
-           || simulationWorkload.useGpuXBufferOps || simulationWorkload.useGpuFBufferOps
-           || simulationWorkload.useGpuHaloExchange || simulationWorkload.useGpuUpdate;
+    return (simulationWorkload.haveGpuPmeOnPpRank()) || simulationWorkload.useGpuXBufferOps
+           || simulationWorkload.useGpuFBufferOps || simulationWorkload.useGpuHaloExchange
+           || simulationWorkload.useGpuUpdate;
 }
 
 class DeviceStreamManager;

commit bb7cb72aac1070b2be4ca1ec46dec3fef9c2836f

Fix MSVC build
Broken in 24c8e7df4f92fcf1caad5b89dd6316d8ef1a647b

src/gromacs/mdtypes/simulation_workload.h
@@ -209,7 +209,11 @@ public:
     bool useMdGpuGraph = false;
 
     // Whether PME GPU is active on this PP rank (note that currently only PP ranks use SimulationWorkload)
-    __attribute__((always_inline)) bool haveGpuPmeOnPpRank() const
+#if !defined(_MSC_VER) // MSVC does not support __attribute__
+    __attribute__((always_inline))
+#endif
+    bool
+    haveGpuPmeOnPpRank() const
     {
         return useGpuPme && !haveSeparatePmeRank;
     }