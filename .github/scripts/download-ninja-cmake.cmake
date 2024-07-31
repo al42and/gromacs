1a5903e3f043a57e7df1d88e0cd6d55fc41bf3c7
Andrey Alekseenko <al42and@gmail.com>
2024-07-31 13:57:25 +0200

    Also bump MSVC, Ninja, CMake

--- .github/scripts/download-ninja-cmake.cmake
+++ .github/scripts/download-ninja-cmake.cmake
@@ -40,12 +40,12 @@ message(STATUS "Using RUNNER_ARCH: $ENV{RUNNER_ARCH}")
 
 if ("$ENV{RUNNER_OS}" STREQUAL "Windows")
     set(ninja_suffix "win.zip")
-    set(cmake_suffix "win64-x64.zip")
-    set(cmake_dir "cmake-${cmake_version}-win64-x64/bin")
+    set(cmake_suffix "windows-x86_64.zip")
+    set(cmake_dir "cmake-${cmake_version}-windows-x86_64/bin")
 elseif ("$ENV{RUNNER_OS}" STREQUAL "macOS")
     set(ninja_suffix "mac.zip")
-    set(cmake_suffix "Darwin-x86_64.tar.gz")
-    set(cmake_dir "cmake-${cmake_version}-Darwin-x86_64/CMake.app/Contents/bin")
+    set(cmake_suffix "macos-universal.tar.gz")
+    set(cmake_dir "cmake-${cmake_version}-macos-universal/CMake.app/Contents/bin")
 elseif ("$ENV{RUNNER_OS}" STREQUAL "Linux" AND "$ENV{RUNNER_ARCH}" STREQUAL "ARM64")
     set(ninja_suffix "linux.zip")
     set(cmake_suffix "linux-aarch64.tar.gz")
@@ -53,12 +53,12 @@ elseif ("$ENV{RUNNER_OS}" STREQUAL "Linux" AND "$ENV{RUNNER_ARCH}" STREQUAL "ARM
 endif()
 
 set(ninja_url "https://github.com/ninja-build/ninja/releases/download/v${ninja_version}/ninja-${ninja_suffix}")
-file(DOWNLOAD "${ninja_url}" ./ninja.zip SHOW_PROGRESS)
+file(DOWNLOAD "${ninja_url}" ./ninja.zip)
 execute_process(COMMAND ${CMAKE_COMMAND} -E tar xvf ./ninja.zip)
 
 set(cmake_url "https://github.com/Kitware/CMake/releases/download/v${cmake_version}/cmake-${cmake_version}-${cmake_suffix}")
-file(DOWNLOAD "${cmake_url}" ./cmake.zip SHOW_PROGRESS)
-execute_process(COMMAND ${CMAKE_COMMAND} -E tar xvf ./cmake.zip)
+file(DOWNLOAD "${cmake_url}" ./cmake.zip)
+execute_process(COMMAND ${CMAKE_COMMAND} -E tar xf ./cmake.zip)
 
 # Add to PATH environment variable
 file(TO_CMAKE_PATH "$ENV{GITHUB_WORKSPACE}/${cmake_dir}" cmake_dir)
